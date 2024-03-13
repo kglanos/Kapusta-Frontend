2d85d3006ffeb8fa1171c362b5c5c5c7d17b53d4
darmen26-dec <dariamendelewska@gmail.com>
2024-03-13 17:41:58 +0100

    small updates - added folders and temporary components

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="Kapusta-Frontend">
+        <BrowserRouter basename="/Kapusta-Frontend">
           <App />
         </BrowserRouter>
       </PersistGate>
