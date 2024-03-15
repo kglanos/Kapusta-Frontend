d8a91caa27c69cd248819b36e56a4105affb92a6
zanetamak <zaneta.zawislak@gmail.com>
2024-03-15 23:52:30 +0100

    first part of changes with login and register form

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,16 +1,20 @@
 import { LoginForm } from '../../components/LoginForm/LoginForm';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useScreenSize } from '../../hooks/useScreenSize'
 
 const Login = () => {
+  const { mobileScreen, tabletScreen, desktopScreen } = useScreenSize(); 
+
   return (
     <HelmetProvider>
       <div>
         <Helmet>
           <title>Login</title>
         </Helmet>
-        <LoginForm />
+        <LoginForm screenSize={{ mobileScreen, tabletScreen, desktopScreen }} /> 
       </div>
     </HelmetProvider>
   );
 };
+
 export default Login;
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,14 +1,17 @@
 import { RegistrationForm } from '../../components/RegistrationForm/RegistrationForm';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useScreenSize } from '../../hooks/useScreenSize'
 
 const Register = () => {
+  const { mobileScreen, tabletScreen, desktopScreen } = useScreenSize();
+
   return (
     <HelmetProvider>
       <div>
         <Helmet>
           <title>Registration</title>
         </Helmet>
-        <RegistrationForm />
+        <RegistrationForm screenSize={{ mobileScreen, tabletScreen, desktopScreen }} />
       </div>
     </HelmetProvider>
   );
