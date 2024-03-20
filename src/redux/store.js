475ad36b04a861d946c0a86ec004416ef1aad7de
darmen26-dec <dariamendelewska@gmail.com>
2024-03-20 18:23:54 +0100

    fixes

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,8 @@ import { authReducer } from './Auth/authSlice';
 import { transactionsReduser } from './Transactions/TransactionsSlice';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
-import { reportsReducer } from './reports/reports.slice';
-import { reportsQueryReducer } from './reportsQuery/reportsQuery.slice';
+import { reportsReducer } from './reports/reportsSlice';
+import { reportsQueryReducer } from './reportsQuery/reportsQuerySlice';
 
 const authPersistConfig = {
   key: 'auth',
