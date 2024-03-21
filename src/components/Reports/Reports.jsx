dfcda1abda98e34d93b818b3f88f5d3a0008aca7
Sebastian <sebaadamoszek@vp.pl>
2024-03-21 22:26:18 +0100

    dodanie date-fns do Date/Date.jsx

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import css from './Reports.module.scss';
 import { BalanceConfirm } from '../BalanceConfirm/BalanceConfirm';
-import { Date } from '../../components/Date/Date';
+import { DateComponent } from '../../components/Date/Date';
 import { Summary } from '../SummaryComponent/SummaryComponent';
 import { ButtonToHome } from '../ButtonToHome/ButtonToHome';
 import { CategoriesListIcon } from '../CategoriesIconsList/CategoriesIconsList';
@@ -13,7 +13,7 @@ export const ReportsPage = () => {
     <HelmetProvider>
       <div className={css.ReportsPageContainer}>
         <ButtonToHome />
-        <Date />
+        <DateComponent />
         <BalanceConfirm />
         <Summary />
         <CategoriesListIcon />
