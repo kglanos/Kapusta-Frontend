07d88c804ba252e0ffdbeda0dab1c4af7cd318ce
Mart <marta.nikiel@yahoo.com>
2024-03-21 12:31:10 +0100

    Update BalanceForm.js

--- src/components/BalanceForm/BalanceForm.js
+++ src/components/BalanceForm/BalanceForm.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ModalBalance from '../../components/ModalStartBalance/ModalStartBalance';
 import { selectBalance } from '../../redux/selectors';
