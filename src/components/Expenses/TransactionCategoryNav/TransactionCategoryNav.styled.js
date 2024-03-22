60f076213e09eb5ed5e9f85b31039e820ac0a6e2
Dawid <dawidpiecha88@gmail.com>
2024-03-22 19:45:28 +0100

    updated links to expenses page and income page in TransactionCategoryNav.jsx

--- src/components/Expenses/TransactionCategoryNav/TransactionCategoryNav.styled.js
+++ src/components/Expenses/TransactionCategoryNav/TransactionCategoryNav.styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const StyledNavMobile = styled.div`
   position: fixed;
@@ -6,45 +7,35 @@ export const StyledNavMobile = styled.div`
   display: flex;
   gap: 4px;
   background-color: #ffffff;
-
-  .NavMobile {
-    display: block;
-    width: 158px;
-    padding: 20px 0;
-    text-decoration: none;
-    text-transform: uppercase;
-    text-align: center;
-    color: #000000;
-    font-weight: 700;
-    background-color: #f5f6fb;
-    :hover,
-    :focus {
-      color: #ffffff;
-      background-color: #ff751d;
-    }
-  }
 `;
-
 export const StyledNavDesktop = styled.div`
   display: flex;
+`;
+export const Link = styled(NavLink)`
+  display: block;
+  width: 158px;
+  padding: 20px 0;
+  text-decoration: none;
+  text-transform: uppercase;
+  text-align: center;
+  color: #000000;
+  font-weight: 700;
+  background-color: #f5f6fb;
 
-  .NavDesktop {
-    display: block;
+  @media screen and (min-width: 768px) {
     width: 138px;
-    padding: 20px 0;
-    text-decoration: none;
-    text-transform: uppercase;
-    text-align: center;
-    color: #000000;
-    font-weight: 700;
     background-color: #fafbfd;
     border-radius: 18px 18px 0 0;
-    :hover,
-    :focus {
-      color: #ff751d;
-      background-color: #fefefe;
-    }
-    &.active {
+  }
+  &:hover,
+  &:focus {
+    color: #ff751d;
+    background-color: #fefefe;
+  }
+  &.active {
+    color: #ffffff;
+    background-color: #ff751d;
+    @media screen and (min-width: 768px) {
       color: #ff751d;
       background-color: #fefefe;
     }
