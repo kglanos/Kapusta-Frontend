53d7ec6842d6174c53898f0ea0d5a521f3caf787
Mart <marta.nikiel@yahoo.com>
2024-03-22 23:07:48 +0100

    Expenses, Incomes, Scroller
    dodanie expenses i incomes z wykorzystaniem getIncomeTransactions i getExpensesTransactions;
    zmieniony scroller ( mam nadzieje, że jak pojawi się zawartość tabeli to pjawi się pomarańczowy scrolller:) )

--- src/components/Expenses/TransactionCategoryNav/TransactionCategoryNav.jsx
+++ src/components/Expenses/TransactionCategoryNav/TransactionCategoryNav.jsx
@@ -23,3 +23,36 @@ export const TransactionNavDesktop = () => {
     </StyledNavDesktop>
   );
 };
+
+// import { useSelector } from 'react-redux';
+// import { getIncomeTransactions } from '../../../redux/Transactions/apiTransactions';
+// import { getExpenseTransactions } from '../../../redux/Transactions/apiTransactions';
+// import {
+//   Container,
+//   Total,
+//   TotalData,
+//   Type,
+// } from './TransactionCategoryNav.styled';
+
+// export const TotalTransactionsData = () => {
+//   const dataIncomes = useSelector(getIncomeTransactions);
+//   const dataExpenses = useSelector(getExpenseTransactions);
+
+//   return (
+//     <Container>
+//       <TotalData>
+//         <Type>Expenses:</Type>
+//         <Total typeReport={'Expenses'}>
+//           {dataExpenses ? `- ${dataExpenses.expenseTotal}` : '0'}
+//         </Total>
+//       </TotalData>
+
+//       <TotalData>
+//         <Type>Income:</Type>
+//         <Total typeReport={'Income'}>
+//           {dataIncomes ? `+ ${dataIncomes.incomeTotal}` : '0'}
+//         </Total>
+//       </TotalData>
+//     </Container>
+//   );
+// };
