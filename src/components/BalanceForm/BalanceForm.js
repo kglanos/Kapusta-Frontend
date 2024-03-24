04f9900ff44ee1728710e4b765bcb5b959f2491b
Mart <marta.nikiel@yahoo.com>
2024-03-25 00:00:25 +0100

    działa niebieski Modal :)

--- src/components/BalanceForm/BalanceForm.js
+++ src/components/BalanceForm/BalanceForm.js
@@ -64,7 +64,7 @@ const BalanceForm = () => {
           <Button type="submit" onClick={handleModalOpen}>
             CONFIRM
           </Button>
-          {balance === '' && <ModalBalance />}
+          {balance === 0 && <ModalBalance />}
           {modalOpen && (
             <Confirm closeModal={handleModalClose} dispatch={handleClick}>
               Are you sure?
@@ -77,3 +77,85 @@ const BalanceForm = () => {
 };
 
 export default BalanceForm;
+
+// import { useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// // import { updateBalance } from '../../redux/Transactions/operations';
+// import { updateBalance } from '../../redux/Auth/operations';
+// import ModalBalance from '../../components/ModalStartBalance/ModalStartBalance';
+// import { Confirm } from '../../components/ModalLogOutAndConfirm/ModalLogOutAndConfirm';
+// import { selectBalance } from '../../redux/selectors';
+// import { useRef } from 'react';
+// import {
+//   Form,
+//   Title,
+//   Input,
+//   Button,
+//   Label,
+//   InputContainer,
+//   WrapperForm,
+// } from './BalanceForm.styled';
+
+// const BalanceForm = () => {
+//   const stateBalance = useSelector(selectBalance);
+//   const dispatch = useDispatch();
+//   const [modalOpen, setModalOpen] = useState(false);
+//   let balance;
+//   const form = useRef();
+
+//   const handleSubmit = evt => {
+//     evt.preventDefault();
+//     balance = evt.target.balance.value;
+//   };
+
+//   const handleClick = () => {
+//     dispatch(updateBalance(balance));
+
+//     // form.current.reset();
+//   };
+
+//   const handleModalOpen = () => {
+//     setModalOpen(true);
+//   };
+
+//   const handleModalClose = () => {
+//     setModalOpen(false);
+//   };
+
+//   return (
+//     <>
+//       <WrapperForm onSubmit={handleSubmit} ref={form}>
+//         <Title htmlFor="balance">Balance:</Title>
+//         <Form>
+//           <InputContainer>
+//             <Input
+//               type="number"
+//               id="balance"
+//               name="balance"
+//               value={balance}
+//               placeholder={`${stateBalance}.00`}
+//               required
+//             />
+//             <Label>USD</Label>
+//           </InputContainer>
+//           <Button type="submit" onClick={handleModalOpen}>
+//             CONFIRM
+//           </Button>
+//           {balance === '' && <ModalBalance />}
+//           {modalOpen && (
+//             <Confirm
+//               changeBalance="true"
+//               closeModal={handleModalClose}
+//               dispatch={handleClick}
+//               balance={balance}
+//             >
+//               Are you sure?
+//             </Confirm>
+//           )}
+//         </Form>
+//       </WrapperForm>
+//     </>
+//   );
+// };
+
+// export default BalanceForm;
